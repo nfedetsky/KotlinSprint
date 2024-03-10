2ae194317ae05e4e291f53e48f4aa59638a34387
Nicholas Fedetsky <nfedetsky@gmail.com>
2024-03-10 22:56:24 +0300

    KS-1-1 variables

---
+++ src/main/kotlin/less_1/less1_task1.kt
@@ -0,0 +1,7 @@
+package org.example.less_1
+
+fun main() {
+    //The first part of the first task. Internet store
+    val productQuant: Byte = 75 //Объявление переменной со значением количества заказов интернет-магазина
+    val gratitudeText: String = "Thanks for your purchase!" //Переменная с текстом благодарности
+}
