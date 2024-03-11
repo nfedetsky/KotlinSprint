90cb5ceb61effeda337fb51aa795db2345d56f08
Nicholas Fedetsky <nfedetsky@gmail.com>
2024-03-12 02:14:54 +0300

    KS-2-4 Calculation part 4 buffs calculation

---
+++ src/main/kotlin/less_2/less2_task4.kt
@@ -0,0 +1,14 @@
+package org.example.less_2
+
+/*
+The buffs calculation
+ */
+fun main() {
+
+    val cristalOre = 7
+    val ironOre = 11
+
+    //Calculation and outputting buffs
+    println("Buff for cristal ore is ${(cristalOre * 0.2).toInt()}")
+    println("Buff for cristal ore is ${(ironOre * 0.2).toInt()}")
+}
