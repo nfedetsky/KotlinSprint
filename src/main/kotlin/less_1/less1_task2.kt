076613b7644231b83755f60b8158662fba6a2380
Nicholas Fedetsky <nfedetsky@gmail.com>
2024-03-11 23:10:23 +0300

    KS-1-2 variables part2 number of employees fixion version

--- src/main/kotlin/less_1/less1_task2.kt
+++ src/main/kotlin/less_1/less1_task2.kt
@@ -4,20 +4,19 @@ fun main() {
 
     val productQuant: Byte = 75 //Объявление переменной со значением количества заказов интернет-магазина
     val gratitudeText: String = "Thanks for your purchase!" //Переменная с текстом благодарности
-    val space = ' '
     var numberOfEmployees: Int = 2000 //Задаем начальное количество работников интернет магазина
 
     //Выводим в консоль количество заказов в магазине
-    println("Currently" + space + "there are" + space + productQuant + space + "orders in the internet store")
+    println("Currently there are space $productQuant orders in the internet store")
     //Выводим в консоль слова благодарности
     println(gratitudeText)
     //Выводим в консоль начальное количество работников магазина
-    println("Currently" + space + "there are" + space + numberOfEmployees + space + "employees in the internet store")
+    //println("Currently there are $numberOfEmployees employees in the internet store")
 
     //Уменьшаем начальное количество работников на одного.
     numberOfEmployees--
 
     //Выводим в консоль новое количество работников магазина
-    println("Oops! No, it's mistake! There are" + space + numberOfEmployees + space + "employees in the internet store. Оne employee just quit us!")
+    println("Oops! No, it's mistake! There are $numberOfEmployees employees in the internet store. Оne employee just quit us!")
 
 }
