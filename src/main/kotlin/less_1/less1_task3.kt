8df3b27fc935b5391eeaab04f44420875d4e78a8
Nicholas Fedetsky <nfedetsky@gmail.com>
2024-03-11 23:24:44 +0300

    KS-1-3 Outputting the start and end times of flight fixion version

--- src/main/kotlin/less_1/less1_task3.kt
+++ src/main/kotlin/less_1/less1_task3.kt
@@ -8,14 +8,13 @@ fun main() {
     val yearOfGFlight = 1961 //The year of Yuri Gagarin's flight
     var hour: String = "09" //The hour og flight. Initial value - our of start
     var minutes: String = "07" //The minutes og flight. Initial value - minutes of start
-    val space = ' '
 
     //Output of the year of flight
-    println("Year of flight:" + space + yearOfGFlight)
+    println("Year of flight: $yearOfGFlight")
     //Output of hour of start flight
-    println("Hour of start flight:" + space + hour)
+    println("Hour of start flight: $hour")
     //Output of minutes of start flight
-    println("Minutes of start flight:" + space + minutes)
+    println("Minutes of start flight: $minutes")
 
     //Enter hour of the end of the flight
     println("Enter hour of the end of the flight")
@@ -26,9 +25,6 @@ fun main() {
     minutes = readln().toString()
 
     //Output time value in one line
-    print("End time of flight:" + space)
-    print(hour)
-    print(":")
-    print(minutes)
+    print("End time of flight: $hour:$minutes")
 
 }
