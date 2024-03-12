fc076bcadeafdfe1dde499438fc8ac505dda53ea
Nicholas Fedetsky <nfedetsky@gmail.com>
2024-03-12 23:21:22 +0300

    KS-1-5 function of calculation of flight time with fixions

--- src/main/kotlin/less_1/less1_task5.kt
+++ src/main/kotlin/less_1/less1_task5.kt
@@ -6,15 +6,20 @@ The function of calculation of flight time
 fun main() {
     //The time variables
     var timeSec = 6480
-    val timeHour: Byte
-    val timeMin: Byte
+    val timeHour: Int
+    val timeMin: Int
 
     //The calculation of hours, minutes and seconds of flight
-    timeHour = (timeSec / 3600).toByte()
-    timeMin = (timeSec / 60 % 60).toByte()
-    timeSec = timeSec / 1 % 60
+    timeHour = (timeSec / SEC_TO_HOUR)
+    timeMin = (timeSec / SEC_TO_MINUTE % SEC_TO_MINUTE)
+    timeSec = timeSec / ONE_SECOND_MOMENT % SEC_TO_MINUTE
 
     //Formatted flight time output
-    println("$timeHour:$timeMin:$timeSec")
+    println(String.format("%02d:%02d:%02d", timeHour, timeMin, timeSec))
 }
+
+//Calculated values
+const val SEC_TO_HOUR = 3600
+const val SEC_TO_MINUTE = 60
+const val ONE_SECOND_MOMENT = 1
 
