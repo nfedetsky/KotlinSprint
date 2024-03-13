ac612972f37cc5dd996efe4e79ae88223f986a6f
Nicholas Fedetsky <nfedetsky@gmail.com>
2024-03-13 21:27:09 +0300

    KS-2-1 GPA calculation fix

--- src/main/kotlin/less_2/less2_task1.kt
+++ src/main/kotlin/less_2/less2_task1.kt
@@ -5,8 +5,13 @@ GPA calculation
  */
 fun main() {
     //Calculation of GPA
-    val gpa = ((3 + 4 + 3 + 5) / 4).toFloat()
+    val gpa = ((STUD_THE_FIRST + STUD_THE_SECOND + STUD_THE_THIRD + STUD_THE_FORTH) / SUM_STUDENTS).toFloat()
 
     //Formatted output
     println(String.format("%.2f", gpa))
 }
+const val STUD_THE_FIRST = 3
+const val STUD_THE_SECOND = 4
+const val STUD_THE_THIRD = 3
+const val STUD_THE_FORTH = 5
+const val SUM_STUDENTS = 4
