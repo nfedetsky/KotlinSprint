56bd0a4cc7aa29197b64b565d5de6964230bfff9
Nicholas Fedetsky <nfedetsky@gmail.com>
2024-03-15 01:48:48 +0300

    KS-3-2 Civil Registry Office

---
+++ src/main/kotlin/less_3/less3_task2.kt
@@ -0,0 +1,24 @@
+package org.example.less_3
+/*
+Civil Registry Office
+ */
+fun main() {
+
+    val maidenName = "Andreeva"
+    val marriedName = "Sidorova"
+    val marriedAge = 22
+    val maidenAge = 20
+    val firstName = "Tatiana"
+    val secondName = "Sergeevna"
+    val announcement_20 = "When Tatyana was young"
+    val announcement_22 = "After wedding"
+
+    //Outputting previous personal data
+    println(announcement_20)
+    println("$maidenName $firstName $secondName $maidenAge\n")
+
+    //Outputting current personal data
+    println(announcement_22)
+    println("$marriedName $firstName $secondName $marriedAge")
+
+}
