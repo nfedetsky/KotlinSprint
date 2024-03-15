8923d40902b9784f6430480f22b8d8a53dae0b16
Nicholas Fedetsky <nfedetsky@gmail.com>
2024-03-15 04:08:31 +0300

    KS-2-4 buffs calculation fix

--- src/main/kotlin/less_2/less2_task4.kt
+++ src/main/kotlin/less_2/less2_task4.kt
@@ -7,8 +7,10 @@ fun main() {
 
     val cristalOre = 7
     val ironOre = 11
+    var buffProcent = 20
 
     //Calculation and outputting buffs
-    println("Buff for cristal ore is ${(cristalOre * 0.2).toInt()}")
-    println("Buff for cristal ore is ${(ironOre * 0.2).toInt()}")
+    println("Buff for cristal ore is ${((cristalOre * buffProcent) / PROCENT_100).toInt()}")
+    println("Buff for cristal ore is ${((ironOre * buffProcent) / PROCENT_100).toInt()}")
 }
+const val PROCENT_100 = 100
