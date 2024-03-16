0f34e6ffbae7e66847db6661755886bf050e2e09
Nicholas Fedetsky <nfedetsky@gmail.com>
2024-03-16 23:38:45 +0300

    KS-3-2 Civil Registry Office fix

--- src/main/kotlin/less_3/less3_task2.kt
+++ src/main/kotlin/less_3/less3_task2.kt
@@ -4,21 +4,19 @@ Civil Registry Office
  */
 fun main() {
 
-    val maidenName = "Andreeva"
-    val marriedName = "Sidorova"
-    val marriedAge = 22
-    val maidenAge = 20
+    var surName = "Andreeva"
+    var brideAge = 20
     val firstName = "Tatiana"
     val secondName = "Sergeevna"
-    val announcement_20 = "When Tatyana was young"
-    val announcement_22 = "After wedding"
 
     //Outputting previous personal data
-    println(announcement_20)
-    println("$maidenName $firstName $secondName $maidenAge\n")
+    println("When Tatyana was young")
+    println("$surName $firstName $secondName $brideAge\n")
 
+    surName = "Sidorova"
+    brideAge = 22
     //Outputting current personal data
-    println(announcement_22)
-    println("$marriedName $firstName $secondName $marriedAge")
+    println("What is Tatiana's surname after wedding?")
+    println("$surName $firstName $secondName $brideAge")
 
 }
