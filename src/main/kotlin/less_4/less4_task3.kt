49dd7cb7cacb4f916b38ddc7129c5b4b3e810f73
Nicholas Fedetsky <nfedetsky@gmail.com>
2024-03-30 11:33:18 +0300

    KS-4-3 Growing beans fix2

--- src/main/kotlin/less_4/less4_task3.kt
+++ src/main/kotlin/less_4/less4_task3.kt
@@ -11,18 +11,18 @@ fun main() {
     val seasonCurrently: String = "Winter"
 
     //Comparison result
-    val comparisonResult = (WETHER_SUNNY == isTodaySunny) &&
+    val comparisonResult = (WEATHER_SUNNY == isTodaySunny) &&
             (TENT_OPEN == isTentOpenCurrently) &&
             (HUMIDITY == humidityToday) &&
-            (SEASON == seasonCurrently)
+            (SEASON != seasonCurrently)
 
     println("$QUESTION $comparisonResult")
 
 }
 
 //Basic data block
-const val WETHER_SUNNY: Boolean = true
+const val WEATHER_SUNNY: Boolean = true
 const val TENT_OPEN: Boolean = true
 const val HUMIDITY: Int = 20
-const val SEASON: String = "Spring"
+const val SEASON: String = "Winter"
 const val QUESTION = "Благоприятные ли условия сейчас для роста бобовых? "
