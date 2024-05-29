15dd519a663541af391a6c2e17afc424e92725e1
Nicholas Fedetsky <nfedetsky@gmail.com>
2024-05-30 00:30:25 +0300

    KS-6-4 The funny game fix2

--- src/main/kotlin/less_6/less6_task4.kt
+++ src/main/kotlin/less_6/less6_task4.kt
@@ -1,19 +1,20 @@
 package org.example.less_6
-
 import kotlin.random.Random
+
 
 /*
 The funny game
  */
 
 fun main() {
-    val hiddenNumber = Random.nextInt(1, 10)
+    val hiddenNumber = Random.extRandomFunction()
     var counter = 5
     val userTries = mapOf(5 to "first", 4 to "second", 3 to "third", 2 to "fourth", 1 to "fifth")
     val userIndexOfTries = mapOf(1 to "one", 2 to "two", 3 to "three", 4 to "four")
     println("Let's play my game. I think of a number from one to nine and You have to guess it.")
     println("You have five tries.")
     println("Let the game begin!")
+
     do {
         println("The ${userTries[counter]} try")
         counter--
@@ -27,4 +28,9 @@ fun main() {
 
     println("I have thought number $hiddenNumber")
     println("Game over!")
+}
+
+fun Random.extRandomFunction(): Int {
+    val randomArgument = nextInt(1, 10)
+    return randomArgument
 }
