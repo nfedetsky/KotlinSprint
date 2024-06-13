fd8e816c7e23ed02b347aec77f5d057fa58fdb20
Nicholas Fedetsky <nfedetsky@gmail.com>
2024-06-14 00:33:21 +0300

    KS-7-3 The even progressions ver.2

--- src/main/kotlin/less_7/less7_task3.kt
+++ src/main/kotlin/less_7/less7_task3.kt
@@ -1,4 +1,5 @@
 package org.example.less_7
+
 /*
 The even progressions
  */
@@ -6,13 +7,6 @@ fun main() {
 
     println("Input Your some number:")
     val userNumber = readln().toInt()
-    val progressionSeries: IntProgression
-
-    if (userNumber < 0) {
-        progressionSeries = 0 downTo userNumber
-        println(progressionSeries.filter { it % 2 == 0 }.joinToString())
-    } else if (userNumber > 0) {
-        progressionSeries = 0..userNumber
-        println(progressionSeries.filter { it % 2 == 0 }.joinToString())
-    } else println("The task has no solution")
+    val progressionSeries = (0..userNumber).step(2).joinToString()
+    println("The even progression is $progressionSeries")
 }
